0bb04b7f8545cc1a8fb44db10cb4afeb968d5ea0
warraich-a <warraich-a@users.noreply.github.com>
2021-06-04 14:37:45 +0200

    Testing PWA

---
+++ serviceWorker.js
@@ -0,0 +1,25 @@
+// const staticDevCoffee = "dev-coffee-site-v1"
+const assets = [
+  "/",
+  "/index.html",
+  "/src/master.css",
+  "/src/PWA.js",
+  "/images/logo.png"
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open("static").then(cache => {
+     return cache.addAll(assets)
+    })
+  )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request)
+    })
+  )
+})
+
