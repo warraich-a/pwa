15a43b54e936f584dd188a2606374e44c72c5a9a
warraich-a <warraich-a@users.noreply.github.com>
2021-06-06 12:14:40 +0200

    Speed test

--- src/PWA.js
+++ src/PWA.js
@@ -4,17 +4,35 @@
 
   
 if ("serviceWorker" in navigator) {
+  const speedX = document.querySelector('#speed');
+  var lastTimestamp;
+  speedY = 0, speedZ = 0;   
   window.addEventListener("load", function() {
     navigator.serviceWorker
       .register('/pwa/serviceWorker.js', {scope: '/pwa/'})
       .then(res => window.addEventListener
         ('devicemotion', function(event) {
-        const speed = document.querySelector('#speed');
-        console.log("Service worker is registered")
-        console.log(event.acceleration.x + ' m/s2');
-        // document.getElementById("speed").innerHTML = "Bonjour";
-        speed.textContent = event.acceleration.x + ' m/s2';
-        }),
+        // const speed = document.querySelector('#speed');
+        // console.log("Service worker is registered")
+        // console.log(event.acceleration.x + ' m/s2');
+        // // document.getElementById("speed").innerHTML = "Bonjour";
+        // speed.textContent = event.acceleration.x + ' m/s2';
+
+        
+        // }
+
+        var currentTime = new Date().getTime();
+        if (lastTimestamp === undefined) {
+          lastTimestamp = new Date().getTime();
+          return; //ignore first call, we need a reference time
+        }
+        //  m/s² / 1000 * (miliseconds - miliseconds)/1000 /3600 => km/h (if I didn't made a mistake)
+        speedX += event.acceleration.x / 1000 * ((currentTime - lastTimestamp)/1000)/3600;
+        //... same for Y and Z
+        lastTimestamp = currentTime;
+      },
+      false),
+           
         geoFindMe()
       )
       .catch(err => console.log("service worker not registeredd", err))
