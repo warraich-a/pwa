89473f8ed897ce1a19d8541ebff11bf6994dc2c3
warraich-a <warraich-a@users.noreply.github.com>
2021-06-07 12:34:16 +0200

    Fixed speed

--- src/PWA.js
+++ src/PWA.js
@@ -50,9 +50,9 @@ function geoFindMe() {
     console.log(position.coords.speed)
 
 
-    status.textContent = '';
-    mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
-    mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+    // status.textContent = '';
+    // mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+    // mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
   }
 
   function error() {
