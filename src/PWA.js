db3c0b95535ed2b6d09663cd473fd04debe639dc
warraich-a <warraich-a@users.noreply.github.com>
2021-06-22 15:42:06 +0200

    Token changed

--- src/PWA.js
+++ src/PWA.js
@@ -1,7 +1,7 @@
 var lastDaySleep
 var fruits;
 function fitBitData(){
-  const access_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzlaRjkiLCJzdWIiOiI5RFhWS0siLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJ3aHIgd3BybyB3bnV0IHdzbGUgd3dlaSB3c29jIHdzZXQgd2FjdCB3bG9jIiwiZXhwIjoxNjI1OTE0MzY3LCJpYXQiOjE2MjMzMjIzNjd9.GEVVbwj2siALHV3HgA3N5JaGuJbz1qwx3sZCHaJThCo"
+  const access_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzlaRjkiLCJzdWIiOiI5RFhWS0siLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJ3aHIgd3BybyB3bnV0IHdzbGUgd3dlaSB3c29jIHdhY3Qgd3NldCB3bG9jIiwiZXhwIjoxNjI1OTE0MzY3LCJpYXQiOjE2MjQzNjkyNzB9.D_r4nXw51vgJ86pMDdXG5lQBrOz4r3YyM0pmN9RRwNc"
 
   fetch('https://api.fitbit.com/1.2/user/-/sleep/list.json?afterDate=2021-03-27&sort=desc&offset=0&limit=4', {
     method: "GET",
